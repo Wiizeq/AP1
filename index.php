4c824af909e2b72e205d6b9f9e6d3e9a4d8b5ceb
Nicolas Kamphaus <nicolas.kamphaus@viacesi.fr>
2018-03-12 15:08:55 +0100

    Ajout parametre HTML

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 include "./includes/header.php";
+
+$page = $_GET['page'] ?? "";
+$page = "./includes/" . $page . ".inc.php";
+
+$files = glob("./includes/*.inc.php");
+
+if (in_array($page, $files))
+    include $page;
+else
+    include "./includes/home.inc.php";
+
+
 include "./includes/footer.php";
+
+
 
